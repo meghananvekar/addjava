226d1820ea38eb4259a39508797708d75b66bfe9
Megha <Megha@gmail.com>
2026-03-14 09:57:09 +0530

    Added add file in java

---
+++ add.java
@@ -0,0 +1,8 @@
+public class AdditionExample {
+   public static void main(String[] args) {
+       int num1 = 10;
+       int num2 = 20;
+       int sum = num1 + num2; // Adding two integers
+       System.out.println("Sum: " + sum); // Output: Sum: 30
+   }
+}
