f954a821ebddc46eb383e626e641e5fca18aa711
meghananvekar <megha.bhat3@gmail.com>
2026-03-14 11:26:37 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,6 @@ public class add {
    public static void main(String[] args) {
        int num1 = 10;
        int num2 = 20;
-       int num3 = 30;
        int sum = num1 + num2 + num3; // Adding two integers
        System.out.println("Sum: " + sum); // Output: Sum: 30
    }
